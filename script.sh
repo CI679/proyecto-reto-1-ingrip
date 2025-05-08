16c5a970e346d0ad0a20d929e094e89eb02c6d2e
g-lazo <a367903@uach.mx>
2025-05-07 18:06:09 -0600

    Lo mismo pero borré dos lineas

--- script.sh
+++ script.sh
@@ -7,10 +7,9 @@ elif [ "$1" = "-t" ]; then
     METHODOLOGY="tradicional"
 elif [ -z "$1" ]; then
     echo "Error: Debe proporcionar un parámetro: '-a' para metodologías ágiles o '-t' para metodologías tradicionales"
-    echo "Uso: $0 [-a|-t]"
     exit 1
 else
     echo "Error: Parámetro inválido. Use '-a' para metodologías ágiles o '-t' para metodologías tradicionales"
-    echo "Uso: $0 [-a|-t]"
     exit 1
 fi
+
